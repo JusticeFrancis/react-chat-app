cd191d6cccbf48e7f761d652877878e8919a28b6
justicefrancis <cjfrancisf@gmail.com>
2022-04-15 00:38:24 +0100

    m

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import { ToastContainer, toast } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
 
-const socket = io(`https://liuyangtong.herokuapp.com`);
+const socket = io(`https://eli-fr.herokuapp.com`);
 
 
 
